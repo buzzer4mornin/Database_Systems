568a86655a7564c6cb75babe12494eeaa2372a7e
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-09-15 17:13:36 +0200

    adding comments

--- mainfile.sql
+++ mainfile.sql
@@ -15,3 +15,11 @@ create table Vehicle(vehicleID int NOT NULL PRIMARY KEY,
 	year_made int,
 	price int,
 	manufactureID int NOT NULL FOREIGN KEY REFERENCES Manufacture(manufactureID) ON DELETE NO ACTION ON UPDATE CASCADE);
+	
+	
+ -- options: "No Action -- Cascade -- Set Null -- Set Default"
+ 
+ -- "ON DELETE NO ACTION" - One of the most safest!! If we try to DELETE "Manufacture" which was referenced on "Vehicle", it will throw an ERROR
+ -- "ON DELETE CASCADE" - Dangerous! Might delete wanted nodes/children...If we try to DELETE "Manufacture", it will also delete "Vehicle" with corresponding "manufactureID"... This operation, deletes all the child nodes which references to parent node.
+ -- "ON DELETE SET NULL" - It changes "manufactureID" value with NULL on Vehicle table, as its corresponding value was deleted on Manufacture table. It can not be labeled as "NOT NULL" when initiated on child table, because we wouldnt be able to SET NULL afterwards..
+ -- "ON DELETE SET DEFAULT" - set it to DEFAULT which was initially set..
