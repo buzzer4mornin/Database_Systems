3a140490b486080630e21fd6ab35440d8c88293b
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-09-15 17:25:29 +0200

    new comments

--- mainfile.sql
+++ mainfile.sql
@@ -30,3 +30,8 @@ create table Spread(distributorID int NOT NULL FOREIGN KEY REFERENCES Distributo
  -- "ON DELETE CASCADE" - Dangerous! Might delete wanted nodes/children...If we try to DELETE "Manufacture", it will also delete "Vehicle" with corresponding "manufactureID"... This operation, deletes all the child nodes which references to parent node.
  -- "ON DELETE SET NULL" - It changes "manufactureID" value with NULL on Vehicle table, as its corresponding value was deleted on Manufacture table. It can not be labeled as "NOT NULL" when initiated on child table, because we wouldnt be able to SET NULL afterwards..
  -- "ON DELETE SET DEFAULT" - set it to DEFAULT which was initially set..
+
+-- "ON UPDATE NO ACTION" - If we try to UPDATE "Manufacture" which was referenced on "Vehicle", it will throw an ERROR saying "you cant update"..
+-- "ON UPDATE CASCADE" - Updates will also be made on child tables..
+-- "ON UPDATE SET NULL" - Sets value to NULL on child tables..
+-- "ON UPDATE SET DEFAULT" - Sets value to DEFAULT on childe tables..
