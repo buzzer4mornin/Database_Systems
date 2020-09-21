a30211a0bdcfced1595947a3fc76cb8b491f7971
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-09-21 17:35:16 +0200

    creating Universal Table

--- final_test.sql
+++ final_test.sql
@@ -1,1 +1,11 @@
 -- Final Exam notes will be here
+Relational Schema Design 
+
+Selecting classes connected with M:N and 1:N --> Manufacture, Vehicle, Distributor
+
+	1. Insert into universal relation table
+	
+		NOTE: Manufacture and Distributor both have attribute "name". Because this can cause confusion, we are changing Manufacture.name -> mname and Distributor.name -> dname
+		
+		Preaparing Universal relation:
+		(vehicleID, model, year_made, price, manufactureID, mname, country, city, average_yearly_capacity,distributorID, dname, average_monthly_distribution_count)
